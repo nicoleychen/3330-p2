017b446ce74a2032b1221b4f1584fbb28436da1a
Aurora Mu <aym32@cornell.edu>
2023-11-13 14:49:53 -0500

    fixed suffolk county and color scheme

--- preprocess.py
+++ preprocess.py
@@ -6,9 +6,9 @@ arcsAll = data['arcs']
 countiesAll = data['objects']['counties']
 want = ["36", "34", "09"]
 countiesGeometry = []
-# namesNY = ['Orange', 'Dutchess', 'Ulster', 'Nassau', 'Suffolk', 'Sullivan', 'Rockland', 'Putnam', 'Passaic', 'Hudson', 'Bergen', 'Westchester', 'Richmond', 'Bronx', 'New York', 'Queens', 'Kings']
-namesNY = ['Orange', 'Dutchess', 'Ulster', 'Nassau', 'Sullivan', 'Rockland', 'Putnam', 'Passaic', 'Hudson', 'Bergen', 'Westchester', 'Richmond', 'Bronx', 'New York', 'Queens', 'Kings']
-namesNJ = ['Middlesex', 'Monmouth', 'Ocean', 'Somerset', 'Essex', 'Union', 'Morris', 'Sussex', 'Hunterdon', 'Mercer']
+namesNY = ['Orange', 'Dutchess', 'Ulster', 'Nassau', 'Suffolk', 'Sullivan', 'Rockland', 'Putnam', 'Westchester', 'Richmond', 'Bronx', 'New York', 'Queens', 'Kings']
+# namesNY = ['Orange', 'Dutchess', 'Ulster', 'Nassau', 'Sullivan', 'Rockland', 'Putnam', 'Westchester', 'Richmond', 'Bronx', 'New York', 'Queens', 'Kings']
+namesNJ = ['Bergen', 'Middlesex', 'Monmouth', 'Ocean', 'Somerset', 'Essex', 'Union', 'Morris', 'Sussex', 'Hunterdon', 'Mercer', 'Passaic', 'Hudson',]
 namesCT = ['Fairfield', 'New Haven', 'Litchfield' ]
 namesAll = namesNY + namesNJ + namesCT
 
@@ -18,20 +18,32 @@ for i in countiesAll['geometries']:
     newGeometry = { 'type': "Polygon", 'arcs': [], 'properties': {} }
     id = i['id'][0:2]
     name = i['properties']['name']
-    if id in want and name in namesAll:
+    # if name == "Suffolk":
+    #     print(i)
+    if id == '36' and name in namesNY: 
+        newProperties['idState'] = 'NY'
         newProperties['name'] = i['properties']['name']
         newProperties['idFIPS'] = id
         newGeometry['arcs'] = i['arcs']
-        if id == '36':
-            newProperties['idState'] = 'NY'
-        elif id == '09':
-            newProperties['idState'] = 'CT'
-        elif id == '34':
-            newProperties['idState'] = 'NJ'
         newGeometry['properties'] = newProperties
         countiesGeometry.append(newGeometry)
-        countiesNew = { 'type': "GeometryCollection", 'geometries': countiesGeometry }
-        dataNew = { 'type': "Topology", 'arcs': arcsAll, 'transform': data['transform'],'objects': { 'counties': countiesNew } }
+    elif id == '09' and name in namesCT:
+        newProperties['idState'] = 'CT'
+        newProperties['name'] = i['properties']['name']
+        newProperties['idFIPS'] = id
+        newGeometry['arcs'] = i['arcs']
+        newGeometry['properties'] = newProperties
+        countiesGeometry.append(newGeometry)
+    elif id == '34' and name in namesNJ:
+        newProperties['idState'] = 'NJ'
+        newProperties['name'] = i['properties']['name']
+        newProperties['idFIPS'] = id
+        newGeometry['arcs'] = i['arcs']
+        newGeometry['properties'] = newProperties
+        countiesGeometry.append(newGeometry)
+countiesNew = { 'type': "GeometryCollection", 'geometries': countiesGeometry }
+dataNew = { 'type': "Topology", 'arcs': arcsAll, 'transform': data['transform'],'objects': { 'counties': countiesNew } }
+# print(dataNew)
 
 with open("countiesTriState.json", "w") as outfile:
     json.dump(dataNew, outfile)
